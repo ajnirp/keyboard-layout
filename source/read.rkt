b1e1a2a7e607ca7d020b93c4eaacba1658c95a60
Rohan Prinja <rohan.prinja@gmail.com>
2013-08-31 02:46:37 +0530

    Added a read method to read corpora

---
+++ source/read.rkt
@@ -0,0 +1,9 @@
+; Reads in a corpus file and returns its text
+; with normalized spaces
+; Docs for string-normalize spaces: Section 3.3 at the bottom
+; String -> String
+(define (read-file corpus-file)
+  (string-normalize-spaces
+   (call-with-input-file
+       corpus-file
+     (λ (input-port) (port->string input-port)))))
